07f358dbadb06bf46eb3e67ab78712763f995efb
mackliu <macklun@gmail.com>
2026-04-21 14:39:00 +0800

    迴圈應用介紹

--- 03-forloop.php
+++ 03-forloop.php
@@ -60,6 +60,44 @@ for($i=0;$i<10;$i=$i+1){
 echo $i;
 
 ?>
+<h3>WHILE loop 練習</h3>
+<?php
+$score=43;
 
+echo "成績為：" . $score . "分 <br>";
+$count=0; //計算迴圈執行次數
+while($score<60){
+    $score=$score+10;
+    $count=$count+1;
+}
+
+echo "登錄成績為：" . $score . "分 <br>";
+echo "迴圈執行次數：" . $count . "次 <br>";
+
+
+?>
+
+<h3>foreach練習</h3>
+<?php 
+$score=[60,22,72,10,80];
+
+foreach($score as $idx => $val){
+    echo '第' . ($idx+1) . '位同學的成績為' . $val . '<br>';
+}
+
+for($i=0;$i<count($score);$i=$i+1){
+
+    echo '第' . ($i+1) . '位同學的成績為' . $score[$i] . '<br>';
+
+}
+
+?>
+
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
+<p>&nbsp;</p>
 </body>
 </html>
